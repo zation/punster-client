5733660ef0c08e483c8005d96bad4978043b4611
Liu Yang <zation1@gmail.com>
2022-07-22 18:08:12 +0800

    add antd

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,6 @@
 import { Link, Outlet } from 'umi';
+import { Button } from 'antd';
+import 'antd/dist/antd.less';
 import styles from './index.less';
 
 export default function Layout() {
@@ -11,6 +13,7 @@ export default function Layout() {
         <li>
           <Link to="/docs">Docs</Link>
         </li>
+        <li><Button>123123</Button></li>
         <li>
           <a href="https://github.com/umijs/umi">Github</a>
         </li>
