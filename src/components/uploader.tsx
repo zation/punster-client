ce8f0174fda2431bc4d66342bce6313e17ad890b
Liu Yang <zation1@gmail.com>
2022-08-05 15:23:53 +0800

    fix upload domain`

--- src/components/uploader.tsx
+++ src/components/uploader.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Upload, UploadProps } from 'antd';
+import { ipfsDomain } from '@/services/ipfs';
 
 export default function Uploader({ onChange, showUploadList, maxCount }: Pick<UploadProps, 'onChange' | 'showUploadList' | 'maxCount'>) {
   return (
     <Upload
       listType="picture-card"
-      action="//47.241.31.74:5001/api/v0/add"
+      action={`${ipfsDomain}:5001/api/v0/add`}
       onChange={onChange}
       showUploadList={showUploadList}
       maxCount={maxCount}
